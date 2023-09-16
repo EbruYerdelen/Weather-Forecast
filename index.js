aa3eb70fb624d1f94a6c71e32d01bc405527106c
Ebru <yerdebru@gmail.com>
2023-09-16 03:02:54 +0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const apiKey = "0f260e939729f0ad8232be1bc4ccce18";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 let searchIcon = document.querySelector(".search-icon");
 let locationBar = document.querySelector("#location");
@@ -10,23 +9,45 @@ let city = document.querySelector(".city");
 let humPercentage = document.querySelector(".percentage");
 let windSpeed = document.querySelector(".speed");
 
-searchIcon.addEventListener("click", getWeather);
+searchIcon.addEventListener("click", runFunc);
 
 async function getWeather() {
   const response = await fetch(apiUrl + locationBar.value + `&appid=${apiKey}`);
-  const data = await response.json();
+  var data = await response.json();
   console.log(data);
 
   city.textContent = data.name;
   temp.textContent = Math.round(data.main.temp) + "°C";
   humPercentage.textContent = data.main.humidity + "%";
   windSpeed.textContent = data.wind.speed + " km/h";
+
   let weatherCondt = data.weather[0].main;
   return weatherCondt;
+
+  /*switch (data.weather[0].main) {
+    case "Clouds":
+      weatherImage.src = "assets/images&design/clouds.png";
+      break;
+    case "Clear":
+      weatherImage.src = "assets/images&design/clear.png";
+      break;
+    case "Drizzle":
+      weatherImage.src = "assets/images&design/drizzle.png";
+      break;
+    case "Mist":
+      weatherImage.src = "assets/images&design/mist.png";
+      break;
+    case "Rain":
+      weatherImage.src = "assets/images&design/rain.png";
+      break;
+    case "Snow":
+      weatherImage.src = "assets/images&design/snow.png";
+      break;
+  }*/
 }
 
-async function displayData(myweather) {
-  switch (myweather) {
+async function displayData(weather) {
+  switch (weather) {
     case "Clouds":
       weatherImage.src = "assets/images&design/clouds.png";
       break;
@@ -48,4 +69,6 @@ async function displayData(myweather) {
   }
 }
 
-getWeather().then(displayData);
+async function runFunc() {
+  await getWeather().then(displayData);
+}
